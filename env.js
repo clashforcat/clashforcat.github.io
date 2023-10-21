7140b1c062721a5b5fe3c1c272902a5ecc5fbef5
clashforcat <148608869+clashforcat@users.noreply.github.com>
2023-10-21 13:42:49 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: 'TomCat',
+  // 站点描述
+  description: '小提示：验证码可能在垃圾箱哦丨Tip: The verification code may be in the trash',
+  // API
+  host: 'https://api.tom66cat.top',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: '',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
